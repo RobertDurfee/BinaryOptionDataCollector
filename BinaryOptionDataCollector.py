ba1dcb686b3664f77c1c5cb943b532022dc91f23
RobertDurfee <robert.durfee.17@outlook.com>
2017-03-18 22:01:31 -0500

    Initial commit.

---
+++ BinaryOptionDataCollector.py
@@ -0,0 +1,35 @@
+import urllib2
+import json
+import mysql.connector
+import time
+
+url = 'http://finance.google.com/finance/info?q=INDEXDJX:.DJI,INDEXSP:.INX,INDEXNASDAQ:.IXIC,INDEXRUSSELL:RUT,INDEXFTSE:XIN0,INDEXFTSE:UKX,INDEXFTSE:WIDEU,INDEXFTSE:JAPAN,CURRENCY:EURUSD,CURRENCY:GBPUSD,CURRENCY:USDJPY,CURRENCY:EURJPY,CURRENCY:AUDUSD,CURRENCY:USDCAD,CURRENCY:GBPJPY,CURRENCY:USDCHF,CURRENCY:EURGBP,CURRENCY:AUDJPY'
+
+cnx = mysql.connector.connect(user='<CENSORED>', password='<CENSORED>', host='<CENSORED>', database='BinaryOptionData')
+
+while True:
+
+    response = urllib2.urlopen(url)
+    html = response.read()
+    
+    html = html[4:] # Remove the prefix '//'
+
+    html = html.decode('utf-8', 'ignore').encode('utf-8') # Remove non-ascii characters
+
+    data = json.loads(html)
+
+    for element in data:
+
+        cursor = cnx.cursor()
+
+        print ("INSERT INTO `" + element['e'] + ":" + element["t"] + "` (`DateTime`, `Price`) VALUES ({}, {})").format(time.strftime("%Y-%m-%d %H:%M:%S", time.strptime(element["lt_dts"], "%Y-%m-%dT%H:%M:%SZ")), element["l"].replace(",", ""))
+
+        cursor.execute("INSERT INTO `" + element['e'] + ":" + element["t"] + "` (`DateTime`, `Price`) VALUES (%s, %s)", (time.strftime("%Y-%m-%d %H:%M:%S", time.strptime(element["lt_dts"], "%Y-%m-%dT%H:%M:%SZ")), element["l"].replace(",", "")))
+        cnx.commit()
+        cursor.close()
+
+        print element['lt'] + ' ' + element['e'] + ":" + element['t'] + ' ' + element['l']
+
+    time.sleep(60)
+
+cnx.close()
